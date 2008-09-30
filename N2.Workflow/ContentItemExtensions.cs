34dbee05c0786c372d622ee5ce1f749700848450
taras <taras@eac458c0-b56b-11dd-a7db-b583d5cdf5c2>
2008-09-30 13:37:25 +0000

    + Implemented default icons for workflow state definitions

---
+++ N2.Workflow/ContentItemExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace N2
+{
+	public static class ContentItemExtensions
+	{
+		/// <summary>
+		/// Generalised version of <see cref="N2.ContentItem"/> SetValue&lt;<typeparamref name="T"/>&gt;(..)
+		/// which may be usefull when the default value is expensive to initialise,
+		/// so it's initialisation is delegated to custom provider.
+		/// </summary>
+		/// <typeparam name="ItemType">The type of reference-type property to retrieve</typeparam>
+		/// <param name="item">Object, for which the property to be retrieved</param>
+		/// <param name="detailName">Property name</param>
+		/// <param name="defaultValueProvider">Delegate that returns default value only when needed.</param>
+		/// <returns>Property value OR if it's NULL THEN the value, returned by a provided delegate.</returns>
+		public static ItemType GetDetail<ItemType>(
+				this ContentItem item,
+				string detailName,
+				Func<ContentItem, ItemType> defaultValueProvider)
+			where ItemType: class//need to be ref type for op?? to work
+		{
+			return
+				item.GetDetail<ItemType>(detailName, default(ItemType))
+				?? (null != defaultValueProvider
+					? defaultValueProvider(item)
+					: default(ItemType));
+		}
+	}
+}
---
+++ N2.Workflow/DefaultIconProvider.cs
@@ -0,0 +1,34 @@
+﻿namespace N2.Workflow
+{
+	using System.Web;
+	using System.Web.UI;
+	using Items;
+	using System.Diagnostics;
+	
+	internal static class DefaultIconProvider
+	{
+		public static string GetIconUrl(
+			this StateDefinition item)
+		{
+			return GetIconUrl((item.SortOrder + 4) % 10 + 1);
+		}
+
+		public static string GetIconUrl(int position)
+		{
+			string _result = null;
+			
+			if (System.Web.Hosting.HostingEnvironment.IsHosted) {
+				Page _page = HttpContext.Current.Handler as Page;
+
+				if (null != _page) {
+					string _res = string.Format("N2.Workflow.Images.{0:00}.png", position);
+					
+					Trace.WriteLine("Resource: "+_res, "Workflow");
+					_result = _page.ClientScript.GetWebResourceUrl(typeof(Workflow), _res);
+				}
+			}
+
+			return _result;
+		}
+	}
+}
