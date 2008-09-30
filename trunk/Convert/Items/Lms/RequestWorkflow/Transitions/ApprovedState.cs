f0375621041561b4efb4a71d966045eada088ca9
taras <taras@eac458c0-b56b-11dd-a7db-b583d5cdf5c2>
2008-09-30 13:43:16 +0000

    Added missing files

---
+++ trunk/Convert/Items/Lms/RequestWorkflow/Transitions/ApprovedState.cs
@@ -0,0 +1,45 @@
+﻿namespace N2.Lms.Items.Lms.RequestStates
+{
+	using System.Linq;
+	using N2.Details;
+	using N2.Integrity;
+	using N2.Workflow.Items;
+	using N2.Lms.Items.TrainingWorkflow;
+
+	[RestrictParents(typeof(Request))]
+	public class ApprovedState: ItemState, IWorkflowItemContainer
+	{
+		[EditableLink("Assign Training", 1, Required = true)]
+		public Training Training {
+			get { return this.GetDetail<Training>("Training", null); }
+			set { this.SetDetail<Training>("Training", value); }
+		}
+
+		TrainingTicket m_ticket;
+		public TrainingTicket Ticket {
+			get { return this.m_ticket ?? (this.m_ticket = this.GetChildren(
+					new N2.Collections
+						.TypeFilter(typeof(TrainingTicket))
+				).FirstOrDefault() as TrainingTicket);
+			}
+		}
+
+		#region IWorkflowItemContainer Members
+
+		public Workflow Workflow { get { return this.Training.Workflow; } }
+
+		#endregion
+
+		#region System properties
+
+		public override string IconUrl
+		{
+			get
+			{
+				return null != this.Ticket ? this.Ticket.IconUrl : base.IconUrl;
+			}
+		}
+		
+		#endregion System properties
+	}
+}
