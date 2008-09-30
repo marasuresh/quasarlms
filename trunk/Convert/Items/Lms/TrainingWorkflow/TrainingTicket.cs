cffb1e75baadc09df6e4ebdba118a37fbcf70b6f
taras <taras@eac458c0-b56b-11dd-a7db-b583d5cdf5c2>
2008-09-30 13:33:03 +0000

    Added missing files

---
+++ trunk/Convert/Items/Lms/TrainingWorkflow/TrainingTicket.cs
@@ -0,0 +1,28 @@
+﻿namespace N2.Lms.Items.TrainingWorkflow
+{
+	using N2.Integrity;
+	using N2.Details;
+	using N2.Workflow;
+	using N2.Lms.Items.Lms.RequestStates;
+	
+	[Definition(Description = @"Represent all training information, associated with a given student.")]
+	[RestrictParents(typeof(Lms.RequestStates.ApprovedState))]
+	[WithWorkflowAction(Name = "Workflow", SortOrder = 3)]
+	[N2.Persistence.NotVersionable]
+	public class TrainingTicket: ContentItem
+	{
+		#region System properties
+
+		public override string IconUrl {
+			get { return this.GetCurrentState().ToState.Icon; }
+		}
+		
+		#endregion System properties
+
+		#region Lms properties
+
+		public Training Training { get { return ((ApprovedState)this.Parent).Training; } }
+
+		#endregion Lms properties
+	}
+}
