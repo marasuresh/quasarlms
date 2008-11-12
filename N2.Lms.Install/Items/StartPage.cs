b7b847bb7bc2a3a8b03f890f7cf13f9a9c9a4fa0
TStrypko <TStrypko@eac458c0-b56b-11dd-a7db-b583d5cdf5c2>
2008-11-12 13:46:29 +0000

    * Updated N2 binaries * Update messaging for a recent changes in N2

--- N2.Lms.Install/Items/StartPage.cs
+++ N2.Lms.Install/Items/StartPage.cs
@@ -1,15 +1,24 @@
 ﻿namespace N2.Lms.Items
 {
+	using N2;
+	using N2.Persistence;
 	using n2 = N2.Templates.Items;
 	using N2.Details;
+	using N2.Web.UI.WebControls;
 	
 	[Definition(
 		"LMS Start Page",
 		"LmsStartPage",
 		"",
 		"", 0)]
-	public class LmsStartPage: n2.StartPage, ILmsStartPage
+	[WithEditable(
+		"Assignment List",
+		typeof(ItemEditorList),
+		"ParentItem", 4, "MyLearningPage")]
+	public class LmsStartPage: n2.StartPage, ILmsStartPage, IActiveContent
 	{
+		#region Pages
+		
 		[EditableItem("MyLearningPage", 3)]
 		public n2.TextPage MyLearningPage { get {
 				return this.GetOrFindOrCreateChild<n2.TextPage>("MyLearningPage", _page => {
@@ -24,10 +33,36 @@
 			}
 		}
 
+		#endregion Pages
+
 		[EditableItem("Storage", 5)]
 		public IStorageItem Storage { get {
 				return this.GetOrFindOrCreateChild<Storage>("Storage", null);
 			}
 		}
+
+		#region IActiveContent Members
+
+		ContentItem N2.Persistence.IActiveContent.CopyTo(ContentItem destination)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		void N2.Persistence.IActiveContent.Delete()
+		{
+			N2.Context.Persister.Delete(this);
+		}
+
+		void N2.Persistence.IActiveContent.MoveTo(ContentItem destination)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		void N2.Persistence.IActiveContent.Save()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		#endregion
 	}
 }
