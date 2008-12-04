8d03725672f5ef4ef63c60b7f9c86e6c5328e57b
TStrypko <TStrypko@eac458c0-b56b-11dd-a7db-b583d5cdf5c2>
2008-12-04 10:45:08 +0000

    * Updated N2 to a recent revision + Added default database backup to App_Data + Added UriTemplateTable support to UriTemplateAttribute

--- trunk/N2.Futures/Web/UriTemplateAttribute.cs
+++ trunk/N2.Futures/Web/UriTemplateAttribute.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace N2.Web
 {
@@ -9,6 +11,7 @@ namespace N2.Web
 
 		readonly string templateUrl;
 		readonly UriTemplate m_template;
+		readonly string action;
 		static Uri BaseUrl = new Uri("http://localhost");
 
 		#endregion Fields
@@ -16,9 +19,15 @@ namespace N2.Web
 		#region Constructors
 
 		public UriTemplateAttribute(string uri, string templateUrl)
+			: this(uri, templateUrl, UriTemplateData.DefaultAction)
+		{
+		}
+
+		public UriTemplateAttribute(string uri, string templateUrl, string actionName)
 		{
 			this.templateUrl = templateUrl;
 			this.m_template = new UriTemplate(uri);
+			this.action = actionName;
 		}
 
 		#endregion Constructors
@@ -27,10 +36,42 @@ namespace N2.Web
 
 		public TemplateData GetTemplate(ContentItem item, string remainingUrl)
 		{
-			var _match = this.m_template.Match(BaseUrl, new Uri(BaseUrl, remainingUrl));
+			var _nextSiblings = (
+				from _attr in item.GetType().GetCustomAttributes(typeof(UriTemplateAttribute), true).Cast<UriTemplateAttribute>()
+				select _attr
+			).ToArray();
 
-			if (null != _match) {
-				return new UriTemplateData(item, templateUrl, _match);
+			if (1 == _nextSiblings.Length) {
+
+				var _firstSibling = _nextSiblings[0];
+
+				var _match = _firstSibling.m_template.Match(
+					BaseUrl,
+					new Uri(BaseUrl, remainingUrl));
+
+				if (null != _match) {
+					return new UriTemplateData(item, _firstSibling.templateUrl, _match);
+				}
+			} else {
+				var _uriTable = new UriTemplateTable(BaseUrl,
+					from _attr in _nextSiblings
+					select new KeyValuePair<UriTemplate, object>(
+						_attr.m_template,
+						_attr)
+					);
+
+				var _matches = _uriTable.Match(new Uri(BaseUrl, remainingUrl));
+
+				if (_matches.Any()) {
+					var _firstMatch = _matches.First();
+					var _matchedAttribute = (UriTemplateAttribute)_firstMatch.Data;
+
+					return new UriTemplateData(
+						item,
+						_matchedAttribute.templateUrl,
+						_matchedAttribute.action,
+						_firstMatch);
+				}
 			}
 
 			return null;
