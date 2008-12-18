c314c93bdba7920aa38added85642706072ba6f5
TStrypko <TStrypko@eac458c0-b56b-11dd-a7db-b583d5cdf5c2>
2008-12-18 13:38:07 +0000

    + Added basic support for item collections via ItemContainer

---
+++ N2.Futures/Items/IItemContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace N2
+{
+	public interface IItemContainer<ItemType>
+	{
+		IEnumerable<ItemType> Items { get; }
+
+		IEnumerable<ItemType> ItemHierarchy { get; }
+	}
+}
